199bba125d41911ae755222166036824ca31de53
Nikolaos Kladis <nikcladis@gmail.com>
2024-01-06 19:38:42 +0200

    added normalize.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
+import "./styles/main.css";
 
 const App: React.FC = () => {
   return (
